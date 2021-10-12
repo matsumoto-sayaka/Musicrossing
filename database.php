1ff2922db15b403a2bf0c3090f4dbbf87c208da5
matsumoto-sayaka <91939385+matsumoto-sayaka@users.noreply.github.com>
2021-10-12 10:13:30 +0900

    Add files via upload

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST',   'localhost'); // データベースのホスト名またはIPアドレス
+define('DB_USER',   'root'); // MySQLのユーザ名
+define('DB_PASSWD', 'root'); // MySQLのパスワード
+define('DB_NAME',   'Musicrossing'); //データベース名
+define('DB_CHARSET','utf8');           // 文字コード
+?>
